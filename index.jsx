f24e8f47be385ea99bcd4ca05b79d67864f4e97e
Xin.Zhang <xin.zhang@wnco.com>
2018-10-14 22:45:48 -0500

    fix refreshing url

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import {
-  Router,
+  BrowserRouter as Router,
   Route,
   Link,
   Switch,
--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,28 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-        {
-          test: /.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: ["es2015"]
-          } 
-        }
-      ]
-  }
+    entry: './index.jsx',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+            test: /.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+                presets: ["es2015", "react"]
+            }
+        }]
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
 };
